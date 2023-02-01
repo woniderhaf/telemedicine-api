4420d9ca43ce592ffe4941d66d1ec4c9f8706e56
woniderhaf <woniderhaf56@mail.ru>
2023-02-01 13:21:59 +0300

    fix 3

--- server.js
+++ server.js
@@ -141,9 +141,9 @@ const publicPath = path.join(__dirname, 'build');
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
 
 server.listen(PORT, () => {
   console.log('Server Started!, port',PORT)
