010f9f805c5102a1ee759376f0539b8e8ceaf041
woniderhaf <woniderhaf56@mail.ru>
2023-02-01 13:06:03 +0300

    fix 2

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ function generateAccessToken(username) {
 }
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
+  console.log({authHeader});
   const token = authHeader && authHeader.split(' ')[1]
 
   if (token == null) return res.sendStatus(401)
