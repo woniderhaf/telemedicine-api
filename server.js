90cb9b418d3f0cf4c5f6421090c248d914c160d7
Kirill Fahredinov <92108744+woniderhaf@users.noreply.github.com>
2023-02-01 12:32:56 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,148 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const cors = require('cors')
+const {version, validate,v4} = require('uuid');
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const ACTIONS = require('./src/socket/actions');
+const PORT = process.env.PORT || 4444;
+
+function getClientRooms() {
+  const {rooms} = io.sockets.adapter;
+  return Array.from(rooms.keys()).filter(roomID => validate(roomID) && version(roomID) === 4);
+}
+app.use(cors())
+app.use(express.json())
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET);
+}
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET , (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+
+app.post('/createRoom', authenticateToken, (rec,res) => {
+  const roomId = v4()
+  res.send({status:'ok', roomId, url:`http://localhost:3000/room/${roomId}`})
+})
+
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOMS, {
+    rooms: getClientRooms()
+  })
+}
+
+io.on('connection', socket => {
+  shareRoomsInfo();
+
+  socket.on(ACTIONS.JOIN, config => {
+    const {room: roomID} = config;
+    console.log('join');
+    const {rooms: joinedRooms} = socket;
+    
+    if (Array.from(joinedRooms).includes(roomID)) {
+      return console.warn(`Already joined to ${roomID}`);
+    }
+
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+        
+    if (clients.length > 1) {
+      socket.emit('ROOM-FULL', {code:501})
+      return console.warn(`room ${roomID} full`);
+    } else {
+      socket.emit('ROOM-FULL', {code:200})
+    }
+
+    clients.forEach(clientID => {
+      io.to(clientID).emit(ACTIONS.ADD_PEER, {
+        peerID: socket.id,
+        createOffer: false
+      });
+
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true,
+      });
+    });
+
+    socket.join(roomID);
+    shareRoomsInfo();
+  });
+
+  function leaveRoom() {
+    const {rooms} = socket;
+    console.log({rooms});
+    Array.from(rooms)
+      // LEAVE ONLY CLIENT CREATED ROOM
+      .filter(roomID => validate(roomID) && version(roomID) === 4)
+      .forEach(roomID => {
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients
+          .forEach(clientID => {
+          io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+            peerID: socket.id,
+          });
+
+          socket.emit(ACTIONS.REMOVE_PEER, {
+            peerID: clientID,
+          });
+        });
+
+        socket.leave(roomID);
+      });
+
+    shareRoomsInfo();
+  }
+
+  socket.on(ACTIONS.LEAVE, config => leaveRoom(config));
+  socket.on('disconnect', leaveRoom);
+
+  socket.on(ACTIONS.RELAY_SDP, ({peerID, sessionDescription}) => {
+    io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription,
+    });
+  });
+
+  socket.on(ACTIONS.RELAY_ICE, ({peerID, iceCandidate}) => {
+    io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    });
+  });
+
+});
+
+const publicPath = path.join(__dirname, 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+server.listen(PORT, () => {
+  console.log('Server Started!, port',PORT)
+})
